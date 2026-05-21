d41e39f64b794d70dc236ac63faa3a35857d3c33
Raphael Sterbac <raphael.sterbac@ens-paris-saclay.fr>
2026-05-21 14:33:12 +0100

    Started messing around with datatypes... (WIP)

--- Datatypes.hs
+++ Datatypes.hs
@@ -14,7 +14,10 @@ import Text.Printf
 import qualified Text.Megaparsec.Char       as C
 import qualified Text.Megaparsec.Char.Lexer as L
 
+-- import Debug.Trace
 
+-- examples
+--------------------------------------------------------------------------------
 
 ex0 = main' "nf" $ unlines [
 
@@ -52,10 +55,10 @@ ex0 = main' "nf" $ unlines [
 -- syntax
 --------------------------------------------------------------------------------
 
--- De Bruijn index.
+-- | De Bruijn index.
 newtype Ix  = Ix  Int deriving (Eq, Show, Num) via Int
 
--- De Bruijn level.
+-- | De Bruijn level.
 newtype Lvl = Lvl Int deriving (Eq, Show, Num) via Int
 
 
@@ -74,10 +77,25 @@ data Raw
 -- core syntax
 ------------------------------------------------------------
 
+data Desc
+  = DescUnit
+  | DescVar
+  | DescTensor Desc Desc
+  | DescSum Name Ty Desc
+  | DescProd Name Ty Desc
+
+
 data Ty
   = Pi Name ~Ty Ty
     | U Int 
     | Decode Int Tm
+    | Unit
+    | Sigma Name ~Ty Ty
+    | Tensor Ty Ty
+    -- Descriptions
+    | Ext Desc Ty
+    | Mu Desc
+    | Square Desc ~Ty Tm
 
 data Tm
   = Var Ix
@@ -85,6 +103,15 @@ data Tm
   | Code Int Ty
   | Lam Name Tm
   | Let Name Ty Tm Tm
+  | Pair Tm Tm
+  | DPair Name Tm Tm
+  | One
+  -- Descriptions 
+  | In Tm
+  | SquareMap Desc Tm Tm
+  | ExtMap Desc Tm Tm
+  | Elim Desc Tm Tm Tm
+
 
 -- values
 ------------------------------------------------------------
@@ -95,36 +122,177 @@ data VTy
   = VPi Name ~VTy (VTm -> VTy)
     | VU Int 
     | VDecode Int VTm
+    | VSigma Name VTy (VTm -> VTy)
+    | VUnit
+    -- Descriptions
+    | VTensor VTy VTy
+    | VSquare VDesc (VTm -> VTy) VTm
+    | VMu Env VDesc
+    
 
 data VTm
   = VVar Lvl
   | VApp VTm ~VTm
   | VCode Int VTy
   | VLam Name (VTm -> VTm)
+  | VPair VTm VTm
+  | VDPair Name VTm VTm
+  | VOne
+  -- Descriptions
+  | VIn VTm
+  | VSquareMap VDesc VTm VTm
+  | VExtMap VDesc VTm VTm
+  | VElim VDesc VTm VTm VTm
+
+data VDesc
+  = VDescUnit
+  | VDescVar
+  | VDescTensor VDesc VDesc
+  | VDescSum Name VTy (VTm -> VDesc)
+  | VDescProd Name VTy (VTm -> VDesc)
 
 --------------------------------------------------------------------------------
+
+vApp :: VTm -> VTm -> VTm
+vApp (VLam _ f) v = f v
+vApp f          v = VApp f v
+
+-- External functions for descriptions
+applyExtMap :: VDesc -> VTm -> VTm -> VTm
+applyExtMap d vf vm = case d of
+  VDescUnit -> VOne
+  
+  VDescVar -> vApp vf vm
+  
+  VDescTensor e1 e2 -> case vm of
+    VPair va vb -> VPair (applyExtMap e1 vf va) (applyExtMap e2 vf vb)
+    k     -> VExtMap d vf k
+    
+  VDescSum n a d1 -> case vm of
+    VDPair n' p1 p2 -> VDPair n' p1 (applyExtMap (d1 p1) vf p2)
+    k         -> VExtMap d vf k
+    
+  VDescProd n a d -> VLam n \v -> applyExtMap (d v) vf (vApp vm v)
+
+applySquareMap :: VDesc -> VTm -> VTm -> VTm
+applySquareMap d vf vm = case d of
+  VDescUnit -> VOne
+  
+  VDescVar -> vApp vf vm
+  
+  VDescTensor d1 d2 -> case vm of
+    VPair v1 v2 -> VPair (applySquareMap d1 vf v1) (applySquareMap d2 vf v2)
+    k     -> VSquareMap d vf k
+    
+  VDescSum n a d1 -> case vm of
+    VDPair _ p1 p2 -> applySquareMap (d1 p1) vf p2
+    k        -> VSquareMap d vf k
+    
+  VDescProd n a d1 -> case vm of
+    VLam n' f -> VLam n' \v -> applySquareMap (d1 v) vf (f v)
+    k   -> VSquareMap d vf k
+
+applyElim :: VDesc -> VTm -> VTm -> VTm -> VTm
+applyElim d vc vm vn = case vn of
+  VIn x -> 
+    let recElim = VLam "_" \v -> applyElim d vc vm v
+        ihs = applySquareMap d recElim x
+    in vApp (vApp vm x) ihs
+    
+  k -> VElim d vc vm k
 
 evalTm :: Env -> Tm -> VTm
 evalTm env = \case
   Var (Ix x)    -> env !! x
-  App t u       -> case (evalTm env t, evalTm env u) of
-                     (VLam _ t, u) -> t u
-                     (t       , u) -> VApp t u
+  App t u       -> vApp (evalTm env t) (evalTm env u)
   Lam x t       -> VLam x \v -> evalTm (v:env) t
   Let x _ t u -> evalTm (evalTm env t : env) u
+  Pair t u      -> VPair (evalTm env t) (evalTm env u)
+  DPair x t u   -> VDPair x (evalTm env t) (evalTm env u)
+  One           -> VOne
+  In t          -> VIn (evalTm env t)
 
   -- Case of a Coding
   Code i a      -> VCode i (evalTy env a)
+  
+  -- Descriptions
+  ExtMap d f m -> 
+    let vf = evalTm env f
+        vm = evalTm env m
+        vd = evalDesc env d
+    in applyExtMap vd vf vm
+    
+  SquareMap d f m -> 
+    let vf = evalTm env f
+        vm = evalTm env m
+        vd = evalDesc env d
+    in applySquareMap vd vf vm
+
+  Elim d c m n ->
+    let vc = evalTm env c
+        vm = evalTm env m
+        vn = evalTm env n
+        vd = evalDesc env d
+    in applyElim vd vc vm vn
+
+applySquare :: Env -> Desc -> (VTm -> VTy) -> VTm -> VTy
+applySquare env d p vm = case d of
+  DescUnit -> VUnit 
+
+  DescVar -> p vm
+
+  DescTensor d e -> case vm of
+    VPair a b -> VTensor (applySquare env d p a) (applySquare env e p b)
+    k     -> VSquare (VDescTensor (evalDesc env d) (evalDesc env e)) p k
+
+  DescSum n a d1 -> case vm of
+    VDPair _ p1 p2 -> applySquare env d1 p p2
+    k        -> VSquare (evalDesc env d) p k
+    
+  DescProd n a d1 ->  VPi n (evalTy env a) \v -> applySquare env d1 p (vApp vm v)
+
 
 evalTy :: Env -> Ty -> VTy
 evalTy env = \case
   Pi x a b      -> VPi x (evalTy env a) \v -> evalTy (v:env) b
   U i           -> VU i
+  Sigma x a b   -> VSigma x (evalTy env a) \v -> evalTy (v:env) b
+  Tensor a b    -> VTensor (evalTy env a) (evalTy env b)
+  Unit          -> VUnit
 
   -- Case of a Decoding
   Decode i t    -> case evalTm env t of
     VCode j a | i == j  -> a             -- Beta Rule for the Universe
     v                   -> VDecode i v
+
+  -- Descriptions
+  Ext d x -> case d of
+    DescUnit -> VUnit 
+    DescVar -> evalTy env x
+    DescTensor d e -> VTensor (evalTy env (Ext d x)) (evalTy env (Ext e x))
+    DescSum n a d -> VSigma n (evalTy env a) \v -> evalTy (v:env) (Ext d x)
+    DescProd n a d -> VPi n (evalTy env a) \v -> evalTy (v:env) (Ext d x) 
+
+  Square d p m ->
+    let vp = \v -> evalTy (v:env) p
+        vm = evalTm env m
+    in applySquare env d vp vm
+
+  Mu d -> VMu env (evalDesc env d)
+
+evalDesc :: Env -> Desc -> VDesc
+evalDesc env = \case
+  DescUnit         -> VDescUnit
+  DescVar          -> VDescVar
+  
+  DescTensor d1 d2 -> 
+    VDescTensor (evalDesc env d1) (evalDesc env d2)
+    
+  DescSum n a d    -> 
+    VDescSum n (evalTy env a) \v -> evalDesc (v:env) d
+    
+  DescProd n a d   -> 
+    VDescProd n (evalTy env a) \v -> evalDesc (v:env) d
 
 
 lvl2Ix :: Lvl -> Lvl -> Ix
@@ -135,17 +303,41 @@ quoteTm l = \case
   VVar x      -> Var (lvl2Ix l x)
   VApp t u    -> App (quoteTm l t) (quoteTm l u)
   VLam x t    -> Lam x (quoteTm (l + 1) (t (VVar l)))
+  VPair t u -> Pair (quoteTm l t) (quoteTm l u)
+  VDPair x t u -> DPair x (quoteTm l t) (quoteTm l u)
+  VOne -> One
 
   -- Case of a Coding
   VCode i a   -> Code i (quoteTy l a)
+
+  -- Descriptions
+  VIn t -> In (quoteTm l t)
+  VSquareMap d f m -> SquareMap (quoteDesc l d) (quoteTm l f) (quoteTm l m)
+  VExtMap d f m -> ExtMap (quoteDesc l d) (quoteTm l f) (quoteTm l m)
+  VElim d c m n -> Elim (quoteDesc l d) (quoteTm l c) (quoteTm l m) (quoteTm l n)
 
 quoteTy :: Lvl -> VTy -> Ty
 quoteTy l = \case
   VPi  x a b  -> Pi x (quoteTy l a) (quoteTy (l + 1) (b (VVar l)))
   VU i        -> U i
+  VSigma x a b -> Sigma x (quoteTy l a) (quoteTy (l + 1) (b (VVar l)))
+  VUnit -> Unit
+  VTensor a b -> Tensor (quoteTy l a) (quoteTy l b)
 
   -- Case of a Decoding
   VDecode i t -> Decode i (quoteTm l t)
+
+  -- Descriptions
+  VSquare d p m -> Square (quoteDesc l d) (quoteTy (l + 1) (p (VVar l))) (quoteTm l m)
+  VMu _ d -> Mu (quoteDesc l d)
+
+quoteDesc :: Lvl -> VDesc -> Desc
+quoteDesc l = \case
+  VDescUnit -> DescUnit
+  VDescVar -> DescVar
+  VDescTensor d1 d2 -> DescTensor (quoteDesc l d1) (quoteDesc l d2)
+  VDescSum n a d -> DescSum n (quoteTy l a) (quoteDesc (l + 1) (d (VVar l)))
+  VDescProd n a d -> DescProd n (quoteTy l a) (quoteDesc (l + 1) (d (VVar l)))
 
 nf :: Env -> Tm -> Tm
 nf env t = quoteTm (Lvl (length env)) (evalTm env t)
@@ -163,20 +355,59 @@ convTm l t u = case (t, u) of
   (VVar x  , VVar x'   ) -> x == x'
   (VApp t u, VApp t' u') -> convTm l t t' && convTm l u u'
 
+  (VPair t1 u1, VPair t2 u2) -> convTm l t1 t2 && convTm l u1 u2
+  (VDPair _ t1 u1, VDPair _ t2 u2) -> convTm l t1 t2 && convTm l u1 u2
+  (VOne, VOne) -> True
+
   (VCode i a, VCode j b) | i == j -> convTy l a b
+
+  (VIn t1, VIn t2) -> convTm l t1 t2
+  (VSquareMap d f m, VSquareMap d' f' m') -> 
+    convDesc l d d' && convTm l f f' && convTm l m m'
+  (VExtMap d f m, VExtMap d' f' m') -> 
+    convDesc l d d' && convTm l f f' && convTm l m m'
+  (VElim d c m n, VElim d' c' m' n') -> 
+    convDesc l d d' && convTm l c c' && convTm l m m' && convTm l n n'
 
   _ -> False
 
 convTy :: Lvl -> VTy -> VTy -> Bool
 convTy l t u = case (t, u) of
   (VU i, VU i') -> i == i'
+  (VUnit, VUnit) -> True
+  (VTensor a b, VTensor a' b') -> convTy l a a' && convTy l b b'
+
 
   (VPi _ a b, VPi _ a' b') ->
        convTy l a a'
     && convTy (l + 1) (b (VVar l)) (b' (VVar l))
 
+
+  (VSigma _ a b, VSigma _ a' b') ->
+       convTy l a a'
+    && convTy (l + 1) (b (VVar l)) (b' (VVar l))
+
   (VDecode i a, VDecode j b) | i == j -> convTm l a b
 
+  -- Descriptions
+  (VSquare d p m, VSquare d' p' m') -> 
+       convDesc l d d' 
+    && convTy (l + 1) (p (VVar l)) (p' (VVar l)) 
+    && convTm l m m'
+  (VMu _ d, VMu _ d') -> convDesc l d d'
+
+  _ -> False
+
+convDesc :: Lvl -> VDesc -> VDesc -> Bool
+convDesc l d d' = case (d, d') of
+  (VDescUnit, VDescUnit) -> True
+  (VDescVar, VDescVar) -> True
+  (VDescTensor a b, VDescTensor a' b') -> 
+    convDesc l a a' && convDesc l b b'
+  (VDescSum _ a b, VDescSum _ a' b') -> 
+    convTy l a a' && convDesc (l + 1) (b (VVar l)) (b' (VVar l))
+  (VDescProd _ a b, VDescProd _ a' b') -> 
+    convTy l a a' && convDesc (l + 1) (b (VVar l)) (b' (VVar l))
   _ -> False
 
 
@@ -186,23 +417,23 @@ convTy l t u = case (t, u) of
 -- type of every variable in scope
 type Types = [(Name, VTy)]
 
--- Elaboration context.
+-- | Elaboration context.
 data Cxt = Cxt {env :: Env, types :: Types, lvl :: Lvl, pos :: SourcePos}
 
 emptyCxt :: SourcePos -> Cxt
 emptyCxt = Cxt [] [] 0
 
--- Extend Cxt with a bound variable.
+-- | Extend Cxt with a bound variable.
 bind :: Name -> VTy -> Cxt -> Cxt
 bind x ~a (Cxt env types l pos) =
   Cxt (VVar l:env) ((x, a):types) (l + 1) pos
 
--- Extend Cxt with a definition.
+-- | Extend Cxt with a definition.
 define :: Name -> VTm -> VTy -> Cxt -> Cxt
 define x ~t ~a (Cxt env types l pos) =
   Cxt (t:env) ((x, a):types) (l + 1) pos
 
--- Typechecking monad. We annotate the error with the current source position.
+-- | Typechecking monad. We annotate the error with the current source position.
 type M = Either (String, SourcePos)
 
 
@@ -213,6 +444,7 @@ report cxt msg = Left (msg, pos cxt)
 
 deriving instance Show Tm
 deriving instance Show Ty
+deriving instance Show Desc
 
 showTm :: Cxt -> Tm -> String
 showTm cxt t = prettyTm 0 (map fst (types cxt)) t []
@@ -258,12 +490,10 @@ coe cxt l sourceTy targetTy m = case (sourceTy, targetTy) of
 
     let env' = env cxt'
     let vu_x = evalTm env' u_x
-    
     let vm = evalTm (env cxt) m
     let vApp = case vm of
                 VLam _ f -> f vu_x
                 _        -> VApp vm vu_x
-                
     let m_u_x = quoteTm l' vApp
     
     n_x <- coe cxt' l' (b1 vu_x) (b2 (VVar l)) m_u_x
@@ -371,20 +601,15 @@ fresh ns x | elem x ns = go (1 :: Int) where
        | otherwise             = x ++ show n
 fresh ns x = x
 
--- printing precedences
 atomp = 3  :: Int -- U, var
 appp  = 2  :: Int -- application
 pip   = 1  :: Int -- pi
 letp  = 0  :: Int -- let, lambda
 
--- | Wrap in parens if expression precedence is lower than
---   enclosing expression precedence.
 par :: Int -> Int -> ShowS -> ShowS
 par p p' = showParen (p' < p)
-
 prettyTm :: Int -> [Name] -> Tm -> ShowS
 prettyTm = goTm where
-
   goTm :: Int -> [Name] -> Tm -> ShowS
   goTm p ns = \case
     Var (Ix x) ->
@@ -392,13 +617,11 @@ prettyTm = goTm where
         "_"   -> ("@"++).(show x++)
         n     -> (n++)
 
-    App t u                   -> par p appp $ goTm appp ns t . (' ':) . goTm atomp ns u
+    App t u -> par p appp $ goTm appp ns t . (' ':) . goTm atomp ns u
 
-    Lam (fresh ns -> x) t     -> par p letp $ ("λ "++) . (x++) . goLam (x:ns) t where
-                                   goLam ns (Lam (fresh ns -> x) t) =
-                                     (' ':) . (x++) . goLam (x:ns) t
-                                   goLam ns t =
-                                     (". "++) . goTm letp ns t
+    Lam (fresh ns -> x) t -> par p letp $ ("λ "++) . (x++) . goLam (x:ns) t where
+      goLam ns (Lam (fresh ns -> x) t) = (' ':) . (x++) . goLam (x:ns) t
+      goLam ns t = (". "++) . goTm letp ns t
 
     Code i t -> ('[':).prettyTy letp ns t.(']':)
 
@@ -406,24 +629,66 @@ prettyTm = goTm where
       par p letp $ ("let "++) . (x++) . (" : "++) . prettyTy letp ns a
       . ("\n    = "++) . goTm letp ns t . ("\n;\n"++) . goTm letp (x:ns) u
 
+    Pair t u -> par p appp $ ("⟨"++) . goTm letp ns t . (", "++) . goTm letp ns u . ("⟩"++)
+
+    DPair _ t u -> par p appp $ ("⟨"++) . goTm letp ns t . (", "++) . goTm letp ns u . ("⟩"++)
+
+    One -> ("*"++)
+
+    In t -> par p appp $ ("in "++) . goTm atomp ns t
+
+    SquareMap d f m -> par p appp $ ("map◻ "++) . prettyDesc atomp ns d . (' ':) . goTm atomp ns f . (' ':) . goTm atomp ns m
+
+    ExtMap d f m -> par p appp $ ("map⟦⟧ "++) . prettyDesc atomp ns d . (' ':) . goTm atomp ns f . (' ':) . goTm atomp ns m
+
+    Elim d c m n -> par p appp $ ("elim "++) . prettyDesc atomp ns d . (' ':) . goTm atomp ns c . (' ':) . goTm atomp ns m . (' ':) . goTm atomp ns n
+
+
 prettyTy :: Int -> [Name] -> Ty -> ShowS
 prettyTy = goTy where
-  piBind ns x a =
-    showParen True ((x++) . (" : "++) . goTy letp ns a)
-
+  piBind ns x a = showParen True ((x++) . (" : "++) . goTy letp ns a)
+  
   goTy :: Int -> [Name] -> Ty -> ShowS
   goTy p ns = \case    
-    U i                       -> par p appp $ ("U "++).(show i++)
+    U i -> par p appp $ ("U "++).(show i++)
 
-    Pi "_" a b                -> par p pip $ goTy appp ns a . (" → "++) . goTy pip ("_":ns) b
+    Pi "_" a b -> par p pip $ goTy appp ns a . (" → "++) . goTy pip ("_":ns) b
 
-    Pi (fresh ns -> x) a b    -> par p pip $ piBind ns x a . goPi (x:ns) b where
-                                   goPi ns (Pi "_" a b) = (" → "++) . goTy appp ns a
-                                                          . (" → "++) . goTy pip ("_":ns) b
-                                   goPi ns (Pi x a b)   = piBind ns x a . goPi (x:ns) b
-                                   goPi ns b            = (" → "++) . goTy pip ns b
+    Pi (fresh ns -> x) a b -> par p pip $ ("Π "++) . piBind ns x a . goPi (x:ns) b where
+      goPi ns (Pi "_" a b) = (". "++) . goTy appp ns a . (" → "++) . goTy pip ("_":ns) b
+      goPi ns (Pi x a b) = piBind ns x a . goPi (x:ns) b
+      goPi ns b = (". "++) . goTy pip ns b
 
-    Decode i t   -> ('<':).prettyTm letp ns t.('>':)
+    Decode i t -> ('<':).prettyTm letp ns t.('>':)
+
+    Unit -> ("1"++)
+
+    Tensor a b -> par p appp $ goTy atomp ns a . (" ⊗ "++) . goTy atomp ns b
+
+    Sigma "_" a b -> par p pip $ goTy appp ns a . (" × "++) . goTy pip ("_":ns) b
+
+    Sigma (fresh ns -> x) a b -> par p pip $ ("Σ "++) . piBind ns x a . (". "++) . goTy pip (x:ns) b
+
+    Ext d x -> par p appp $ ("⟦ "++) . prettyDesc letp ns d . (" ⟧ "++) . goTy atomp ns x
+
+    Mu d -> par p appp $ ("μ "++) . prettyDesc atomp ns d
+
+    Square d pr m -> par p appp $ ("◻ "++) . prettyDesc atomp ns d . (' ':) . goTy atomp ("_":ns) pr . (' ':) . prettyTm atomp ns m
+
+
+prettyDesc :: Int -> [Name] -> Desc -> ShowS
+prettyDesc = goDesc where
+  goDesc :: Int -> [Name] -> Desc -> ShowS
+  goDesc p ns = \case
+    DescUnit -> ("1"++)
+
+    DescVar -> ("X"++)
+
+    DescTensor d1 d2 -> par p appp $ goDesc atomp ns d1 . (" ⊗ "++) . goDesc atomp ns d2
+
+    DescSum (fresh ns -> x) a d -> par p pip $ ("Σ "++) . showParen True ((x++) . (" : "++) . prettyTy letp ns a) . (". "++) . goDesc pip (x:ns) d
+
+    DescProd (fresh ns -> x) a d -> par p pip $ ("Π "++) . showParen True ((x++) . (" : "++) . prettyTy letp ns a) . (". "++) . goDesc pip (x:ns) d
 
 -- instance Show Tm where showsPrec p = prettyTm p []
 
