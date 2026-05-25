dd335bbb57b6eb86714bd8af122f79e2f9bcae81
Raphael Sterbac <raphael.sterbac@ens-paris-saclay.fr>
2026-05-25 16:48:50 +0100

    Added labeled descriptions and other things related to enumerations

--- Datatypes.hs
+++ Datatypes.hs
@@ -90,12 +90,16 @@ data Raw
 -- core syntax
 ------------------------------------------------------------
 
+data Label 
+  = Data Name [Tm]
+
 data Desc
   = DescUnit
   | DescVar
   | DescTensor Desc Desc
   | DescSum Name Ty Desc
   | DescProd Name Ty Desc
+  | DescCall Label Tm 
 
 
 data Ty
@@ -109,7 +113,7 @@ data Ty
     | Ext Desc Ty
     | Mu Desc
     | Square Desc ~Ty Tm
-    | Label Name
+    | DLabel Label
     -- Enumerations
     | EnumU
     | EnumT Tm
@@ -129,6 +133,7 @@ data Tm
   | SquareMap Desc Tm Tm
   | ExtMap Desc Tm Tm
   | Elim Desc Tm Tm Tm
+  | DReturn Desc
   -- Enumerations
   | NilE
   | ConsE Name Tm
@@ -152,6 +157,7 @@ data VTy
     | VTensor VTy VTy
     | VSquare VDesc (VTm -> VTy) VTm
     | VMu Env VDesc
+    | VDLabel Label
     -- Enumerations
     | VEnumU
     | VEnumT VTm
@@ -171,6 +177,7 @@ data VTm
   | VSquareMap VDesc VTm VTm
   | VExtMap VDesc VTm VTm
   | VElim VDesc VTm VTm VTm
+  | VDReturn VDesc
     -- Enumerations
   | VNilE
   | VConsE Name VTm
@@ -184,6 +191,7 @@ data VDesc
   | VDescTensor VDesc VDesc
   | VDescSum Name VTy (VTm -> VDesc)
   | VDescProd Name VTy (VTm -> VDesc)
+  | VDescCall Label VTm
 
 --------------------------------------------------------------------------------
 
@@ -283,6 +291,8 @@ evalTm env = \case
         vd = evalDesc env d
     in applyElim vd vc vm vn
 
+  DReturn d -> VDreturn evalDesc d
+
   -- Enumerations
   NilE       -> VNilE
   ConsE n t  -> VConsE n (evalTm env t)
@@ -337,6 +347,8 @@ evalTy env = \case
     in applySquare env d vp vm
 
   Mu d -> VMu env (evalDesc env d)
+
+  DLabel l -> VDLabel l
   
   -- Enumerations
   EnumU        -> VEnumU
@@ -348,14 +360,15 @@ evalDesc env = \case
   DescUnit         -> VDescUnit
   DescVar          -> VDescVar
   
-  DescTensor d1 d2 -> 
-    VDescTensor (evalDesc env d1) (evalDesc env d2)
+  DescTensor d1 d2 -> VDescTensor (evalDesc env d1) (evalDesc env d2)
     
-  DescSum n a d    -> 
-    VDescSum n (evalTy env a) \v -> evalDesc (v:env) d
+  DescSum n a d    -> VDescSum n (evalTy env a) \v -> evalDesc (v:env) d
     
-  DescProd n a d   -> 
-    VDescProd n (evalTy env a) \v -> evalDesc (v:env) d
+  DescProd n a d   -> VDescProd n (evalTy env a) \v -> evalDesc (v:env) d
+
+  DescCall l t -> case (evalTm env t) of
+    VDReturn d -> d
+    k -> VDescCall l k
 
 
 lvl2Ix :: Lvl -> Lvl -> Ix
@@ -378,6 +391,14 @@ quoteTm l = \case
   VSquareMap d f m -> SquareMap (quoteDesc l d) (quoteTm l f) (quoteTm l m)
   VExtMap d f m -> ExtMap (quoteDesc l d) (quoteTm l f) (quoteTm l m)
   VElim d c m n -> Elim (quoteDesc l d) (quoteTm l c) (quoteTm l m) (quoteTm l n)
+  VDReturn d -> DReturn (quoteDesc l d)
+
+  -- Enumerations
+  VNilE -> NilE
+  VConsE n t -> ConsE n (quoteTm l t)
+  VZeroE -> ZeroE
+  VSuccE t -> SuccE (quoteTm l t)
+  VSwitch t u -> Switch (quoteTm l t) (quoteTm l u)
 
 quoteTy :: Lvl -> VTy -> Ty
 quoteTy l = \case
@@ -393,6 +414,12 @@ quoteTy l = \case
   -- Descriptions
   VSquare d p m -> Square (quoteDesc l d) (quoteTy (l + 1) (p (VVar l))) (quoteTm l m)
   VMu _ d -> Mu (quoteDesc l d)
+  VDLabel l -> DLabel l
+
+  -- Enumerations
+  VEnumU -> EnumU
+  VEnumT t -> EnumT (quoteTm l t)
+  VSmallPiE t u -> SmallPiE (quoteTm l t) (quoteTm l u)
 
 quoteDesc :: Lvl -> VDesc -> Desc
 quoteDesc l = \case
@@ -401,19 +428,17 @@ quoteDesc l = \case
   VDescTensor d1 d2 -> DescTensor (quoteDesc l d1) (quoteDesc l d2)
   VDescSum n a d -> DescSum n (quoteTy l a) (quoteDesc (l + 1) (d (VVar l)))
   VDescProd n a d -> DescProd n (quoteTy l a) (quoteDesc (l + 1) (d (VVar l)))
+  VDescCall l t -> DescCall l (quoteTm l t)
 
 nf :: Env -> Tm -> Tm
 nf env t = quoteTm (Lvl (length env)) (evalTm env t)
 
 convTm :: Lvl -> VTm -> VTm -> Bool
 convTm l t u = case (t, u) of
-  (VLam _ t, VLam _ t') ->
-    convTm (l + 1) (t (VVar l)) (t' (VVar l))
+  (VLam _ t, VLam _ t') -> convTm (l + 1) (t (VVar l)) (t' (VVar l))
 
-  (VLam _ t, u) ->
-    convTm (l + 1) (t (VVar l)) (VApp u (VVar l))
-  (u, VLam _ t) ->
-    convTm (l + 1) (VApp u (VVar l)) (t (VVar l))
+  (VLam _ t, u) -> convTm (l + 1) (t (VVar l)) (VApp u (VVar l))
+  (u, VLam _ t) -> convTm (l + 1) (VApp u (VVar l)) (t (VVar l))
 
   (VVar x  , VVar x'   ) -> x == x'
   (VApp t u, VApp t' u') -> convTm l t t' && convTm l u u'
@@ -424,13 +449,19 @@ convTm l t u = case (t, u) of
 
   (VCode i a, VCode j b) | i == j -> convTy l a b
 
+  -- Descriptions
   (VIn t1, VIn t2) -> convTm l t1 t2
-  (VSquareMap d f m, VSquareMap d' f' m') -> 
-    convDesc l d d' && convTm l f f' && convTm l m m'
-  (VExtMap d f m, VExtMap d' f' m') -> 
-    convDesc l d d' && convTm l f f' && convTm l m m'
-  (VElim d c m n, VElim d' c' m' n') -> 
-    convDesc l d d' && convTm l c c' && convTm l m m' && convTm l n n'
+  (VSquareMap d f m, VSquareMap d' f' m') -> convDesc l d d' && convTm l f f' && convTm l m m'
+  (VExtMap d f m, VExtMap d' f' m') -> convDesc l d d' && convTm l f f' && convTm l m m'
+  (VElim d c m n, VElim d' c' m' n') -> convDesc l d d' && convTm l c c' && convTm l m m' && convTm l n n'
+  (VDReturn d, VDReturn d') -> convDesc l d d'
+
+  -- Enumerations
+  (VNilE, VNilE) -> True
+  (VConsE _ t, VConsE _ t') -> convTm l t t'
+  (VZeroE, VZeroE) -> True
+  (VSuccE t, VSuccE t') -> convTm l t t'
+  (VSwitch t u, VSwitch t' u') -> convTm l t t' && convTm l u u'
 
   _ -> False
 
@@ -441,23 +472,19 @@ convTy l t u = case (t, u) of
   (VTensor a b, VTensor a' b') -> convTy l a a' && convTy l b b'
 
 
-  (VPi _ a b, VPi _ a' b') ->
-       convTy l a a'
-    && convTy (l + 1) (b (VVar l)) (b' (VVar l))
-
-
-  (VSigma _ a b, VSigma _ a' b') ->
-       convTy l a a'
-    && convTy (l + 1) (b (VVar l)) (b' (VVar l))
-
+  (VPi _ a b, VPi _ a' b') -> convTy l a a' && convTy (l + 1) (b (VVar l)) (b' (VVar l))
+  (VSigma _ a b, VSigma _ a' b') -> convTy l a a' && convTy (l + 1) (b (VVar l)) (b' (VVar l))
   (VDecode i a, VDecode j b) | i == j -> convTm l a b
 
   -- Descriptions
-  (VSquare d p m, VSquare d' p' m') -> 
-       convDesc l d d' 
-    && convTy (l + 1) (p (VVar l)) (p' (VVar l)) 
-    && convTm l m m'
+  (VSquare d p m, VSquare d' p' m') -> convDesc l d d'  && convTy (l + 1) (p (VVar l)) (p' (VVar l))  && convTm l m m'
   (VMu _ d, VMu _ d') -> convDesc l d d'
+  (VDLabel (Data n _), VDLabel (Data n' _)) -> n == n' -- TODO : Maybe I should also try to equate the parameters here, not sure
+
+  -- Enumerations
+  (VEnumU, VEnumU) -> True
+  (VEnumT t, VEnumT t') -> convTm l t t'
+  (VSmallPiE t u, VSmallPiE t' u') -> convTm l t t' && convTm l u u'
 
   _ -> False
 
@@ -465,12 +492,10 @@ convDesc :: Lvl -> VDesc -> VDesc -> Bool
 convDesc l d d' = case (d, d') of
   (VDescUnit, VDescUnit) -> True
   (VDescVar, VDescVar) -> True
-  (VDescTensor a b, VDescTensor a' b') -> 
-    convDesc l a a' && convDesc l b b'
-  (VDescSum _ a b, VDescSum _ a' b') -> 
-    convTy l a a' && convDesc (l + 1) (b (VVar l)) (b' (VVar l))
-  (VDescProd _ a b, VDescProd _ a' b') -> 
-    convTy l a a' && convDesc (l + 1) (b (VVar l)) (b' (VVar l))
+  (VDescTensor a b, VDescTensor a' b') -> convDesc l a a' && convDesc l b b'
+  (VDescSum _ a b, VDescSum _ a' b') -> convTy l a a' && convDesc (l + 1) (b (VVar l)) (b' (VVar l))
+  (VDescProd _ a b, VDescProd _ a' b') -> convTy l a a' && convDesc (l + 1) (b (VVar l)) (b' (VVar l))
+  (VDescCall (Data n _) t, VDescCall (Data n' _) t') -> n == n' && convTm l t t' -- TODO : Maybe I should also try to equate the parameters here, not sure
   _ -> False
 
 
@@ -709,6 +734,16 @@ prettyTm = goTm where
 
     Elim d c m n -> par p appp $ ("elim "++) . prettyDesc atomp ns d . (' ':) . goTm atomp ns c . (' ':) . goTm atomp ns m . (' ':) . goTm atomp ns n
 
+    NilE -> ("nil"++)
+
+    ConsE n t -> par p appp $ ("cons "++) . (n++) . (' ':) . goTm atomp ns t
+
+    ZeroE -> ("0"++)
+
+    SuccE t -> par p appp $ ("1+ "++) . goTm atomp ns t
+
+    Switch t u -> par p appp $ ("switch "++) . goTm atomp ns t . (' ':) . goTm atomp ns u
+
 
 prettyTy :: Int -> [Name] -> Ty -> ShowS
 prettyTy = goTy where
@@ -741,6 +776,13 @@ prettyTy = goTy where
 
     Square d pr m -> par p appp $ ("◻ "++) . prettyDesc atomp ns d . (' ':) . goTy atomp ("_":ns) pr . (' ':) . prettyTm atomp ns m
 
+    DLabel n -> (n++)
+
+    EnumU -> ("EnumU "++)
+
+    EnumT t -> par p appp $ ("Enum "++) . prettyTm atomp ns t
+
+    SmallPiE t u -> par p appp $ ("π_E "++) . prettyTm atomp ns t . (' ':) . prettyTm atomp ns u
 
 prettyDesc :: Int -> [Name] -> Desc -> ShowS
 prettyDesc = goDesc where
